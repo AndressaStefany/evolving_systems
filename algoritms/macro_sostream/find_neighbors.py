a23b4cf8d574d302c815d77e848a127c0a2de231
AndressaStefany <astefanysoliveira@gmail.com>
2020-09-25 10:54:01 -0300

    Change portuguese>english

--- algoritms/macro_sostream/find_neighbors.py
+++ algoritms/macro_sostream/find_neighbors.py
@@ -8,23 +8,23 @@ def find_neighbors(win_microcluster, min_pts, model_t, all_mclusters):
         win_dist = []
         all_win_dist = []
         for microcluster in model_t:
-            # calculando todas as distancias do vencedor para os vizinhos
+            # calculating all distances from the winner to the neighbors
             win_dist.append(dist(microcluster.centroid,
                                  win_microcluster.centroid))
 
         for microcluster in all_mclusters:
-            # calculando todas as distancias do vencedor para os vizinhos
+            # calculating all distances from the winner to the neighbors
             all_win_dist.append(dist(microcluster.centroid,
                                      win_microcluster.centroid))
-        # ordem da menor para a maior distancia, referencia o vencedor
+        # order from the smallest to the largest distance, references the winner
         win_dist.sort()
         all_win_dist.sort()
-        # indeces que ordenam o array win_dist
+        # indexes that sort the win_dist array
         idx_microclusters = np.argsort(win_dist)
 
-        # k_dist é a metade da distância entre o vencedor e
-        # um microcluster mais proximo
-        k_dist = all_win_dist[min_pts-1]  # mudado
+        # k_dist is half the distance between the winner
+        # and the nearest microcluster
+        k_dist = all_win_dist[min_pts-1]
         win_microcluster.radius = k_dist
         win_nn = [model_t[idx] for idx in idx_microclusters[0:(min_pts)]]
         return win_nn
